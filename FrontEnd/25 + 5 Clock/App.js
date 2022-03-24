8f23902f4e0d6ea33610584f371e9fe5091d3364
IqroNegoro <iqronegoro0@gmail.com>
2022-03-24 20:42:59 +0700

    Mulai Buat Component

--- FrontEnd/25 + 5 Clock/App.js
+++ FrontEnd/25 + 5 Clock/App.js
@@ -1,1 +1,20 @@
+class Timer extends React.Component {
 
+}
+
+let TimerLengthControls = props => {
+    return (
+        <div className={props.label}>
+            <p>{props.title}</p>
+            <div id={props.labelInc}>
+                <i class="bx bx-up-arrow-alt"></i>
+            </div>
+            <div id={props.labelLength}>
+                {props.length}
+            </div>
+            <div id={props.labelDec}>
+                <i class="bx bx-down-arrow-alt"></i>
+            </div>
+        </div>
+    )
+}
